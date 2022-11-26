7498cd2d82351e14eb9b840e587a03860344b5ab
Anton Zmiievskyi <anton.zmievsky@gmail.com>
2022-11-26 13:32:11 +0200

    dd

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,22 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import React, {Component} from "react";
+
+export class App {
+
+  render(){
+return (
+  <div
+    style={{
+      height: '100vh',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      fontSize: 40,
+      color: '#010101',
+    }}
+  >
+    React homework template
+  </div>
+);
+  }
+  
 };
