777c395ac99ef0db8f04aff50137db91a39ccea7
Anton Zmiievskyi <anton.zmievsky@gmail.com>
2022-11-26 13:46:50 +0200

    s

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,6 @@
-import React, {Component} from "react";
+import React from "react";
 
-export class App {
+class App {
 
   render(){
 return (
@@ -20,3 +20,6 @@ return (
   }
   
 };
+
+
+export default App
