d9267150ebcb5a76dbc1a07802b1e175075ad823
Anton Zmiievskyi <anton.zmievsky@gmail.com>
2022-11-26 13:39:23 +0200

    f

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App/App';
 import { ThemeProvider } from 'styled-components';
-import { theme } from './components/Theme';
+import { theme } from './theme';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
